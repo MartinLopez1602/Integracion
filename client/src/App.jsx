ac69c0c37eaa283abb57448ef13e9db70fe18c8f
Martin Lopez <martinantulopez@gmail.com>
2025-05-21 16:39:27 -0400

    Implementacion webpay con respueta simulada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,9 @@ import Pedidos from './pages/Pedidos';
 import Contacto from './pages/Contacto';
 import Home from './pages/Home';
 import Carrito from './pages/Carrito';
+import PagoExitoso from './pages/PagoExitoso';
+import PagoFallido from './pages/PagoFallido';
+import WebpaySimulator from './pages/WebpaySimulator';
 
 // enruta las paginas, como se hace en Angular, copia y pega otro nomas y le cambias el nombre
 function App() {
@@ -23,6 +26,9 @@ function App() {
         <Route path="/pedidos" element={<Pedidos />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/carrito" element={<Carrito />} />
+        <Route path="/pago-exitoso" element={<PagoExitoso />} />
+        <Route path="/pago-fallido" element={<PagoFallido />} />
+        <Route path="/webpay-simulator" element={<WebpaySimulator />} />
       </Routes>
     </CartProvider>
   );
