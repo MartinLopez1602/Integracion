d56516735f7b8e4227e6668586b22f56c50571fe
Luke-PC <luka.martinez@duocuc.cl>
2025-07-02 21:28:32 -0400

    no borre el slash soy tonto lol hostia

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ if (!isTestMode) console.log('Configurando middlewares...');
 // CORS configurado correctamente para local y producción
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://integracion-five.vercel.app/' // NO CAMBIAR, SOLO SI SE ACTUALIZA EL DOMINIO aa
+  'https://integracion-five.vercel.app' // NO CAMBIAR, SOLO SI SE ACTUALIZA EL DOMINIO aa
 ];
 
 app.use(cors({
