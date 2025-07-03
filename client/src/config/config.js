728ebcbec9613712759b131dcd6a8dbe114f37d5
Luke-PC <luka.martinez@duocuc.cl>
2025-07-02 20:44:03 -0400

    actualizacion del dominio

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 // para cambiar la urls en base en que enviroment
 const configs = {
   development: 'http://localhost:5000',
-  production: 'https://ferremas-app-env-2.eba-dqgxevfn.us-east-1.elasticbeanstalk.com'
+  production: 'backend-production-51bf.up.railway.app'
 };
 
 // Asegúrate de que NODE_ENV esté correctamente establecido durante el build
